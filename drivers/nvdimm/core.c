5c413d1aa4fd33fb698309daf68baf12a5873c15
Dan Williams <dan.j.williams@intel.com>
2022-04-20 19:36:02 -0700

    nvdimm: Fix firmware activation deadlock scenarios
    Lockdep reports the following deadlock scenarios for CXL root device
    power-management, device_prepare(), operations, and device_shutdown()
    operations for 'nd_region' devices:
    
    ---
     Chain exists of:
       &nvdimm_region_key --> &nvdimm_bus->reconfig_mutex --> system_transition_mutex
    
      Possible unsafe locking scenario:
    
            CPU0                    CPU1
            ----                    ----
       lock(system_transition_mutex);
                                    lock(&nvdimm_bus->reconfig_mutex);
                                    lock(system_transition_mutex);
       lock(&nvdimm_region_key);
    
    --
    
     Chain exists of:
       &cxl_nvdimm_bridge_key --> acpi_scan_lock --> &cxl_root_key
    
      Possible unsafe locking scenario:
    
            CPU0                    CPU1
            ----                    ----
       lock(&cxl_root_key);
                                    lock(acpi_scan_lock);
                                    lock(&cxl_root_key);
       lock(&cxl_nvdimm_bridge_key);
    
    ---
    
    These stem from holding nvdimm_bus_lock() over hibernate_quiet_exec()
    which walks the entire system device topology taking device_lock() along
    the way. The nvdimm_bus_lock() is protecting against unregistration,
    multiple simultaneous ops callers, and preventing activate_show() from
    racing activate_store(). For the first 2, the lock is redundant.
    Unregistration already flushes all ops users, and sysfs already prevents
    multiple threads to be active in an ops handler at the same time. For
    the last userspace should already be waiting for its last
    activate_store() to complete, and does not need activate_show() to flush
    the write side, so this lock usage can be deleted in these attributes.
    
    Fixes: 48001ea50d17 ("PM, libnvdimm: Add runtime firmware activation support")
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/nvdimm/core.c
+++ drivers/nvdimm/core.c
@@ -395,10 +395,8 @@ static ssize_t activate_show(struct device *dev,
 	if (!nd_desc->fw_ops)
 		return -EOPNOTSUPP;
 
-	nvdimm_bus_lock(dev);
 	cap = nd_desc->fw_ops->capability(nd_desc);
 	state = nd_desc->fw_ops->activate_state(nd_desc);
-	nvdimm_bus_unlock(dev);
 
 	if (cap < NVDIMM_FWA_CAP_QUIESCE)
 		return -EOPNOTSUPP;
@@ -443,7 +441,6 @@ static ssize_t activate_store(struct device *dev,
 	else
 		return -EINVAL;
 
-	nvdimm_bus_lock(dev);
 	state = nd_desc->fw_ops->activate_state(nd_desc);
 
 	switch (state) {
@@ -461,7 +458,6 @@ static ssize_t activate_store(struct device *dev,
 	default:
 		rc = -ENXIO;
 	}
-	nvdimm_bus_unlock(dev);
 
 	if (rc == 0)
 		rc = len;
