ddbb18e88c9b6cb4e1d8cd09e57ebaa92f8923e4
Dan Williams <dan.j.williams@intel.com>
2023-03-21 15:38:26 -0700

    cxl/region: Specify host-only vs device memory at region creation time
    In preparation for supporting device-memory (HDM-D[B]) region creation,
    convey the endpoint-decoder target type to devm_cxl_add_region().
    
    Note that none of the existing sysfs ABIs allow for HDM-D[B] region
    creation. The expectation is that HDM-D[B] region creation requires a
    kernel-internal region creation flow, for example, driven by an
    accelerator driver.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -2121,7 +2121,8 @@ static ssize_t create_ram_region_show(struct device *dev,
 }
 
 static struct cxl_region *__create_region(struct cxl_root_decoder *cxlrd,
-					  enum cxl_decoder_mode mode, int id)
+					  int id, enum cxl_decoder_mode mode,
+					  enum cxl_decoder_type type)
 {
 	int rc;
 
@@ -2134,7 +2135,7 @@ static struct cxl_region *__create_region(struct cxl_root_decoder *cxlrd,
 		return ERR_PTR(-EBUSY);
 	}
 
-	return devm_cxl_add_region(cxlrd, id, mode, CXL_DECODER_HOSTMEM);
+	return devm_cxl_add_region(cxlrd, id, mode, type);
 }
 
 static ssize_t create_pmem_region_store(struct device *dev,
@@ -2149,7 +2150,8 @@ static ssize_t create_pmem_region_store(struct device *dev,
 	if (rc != 1)
 		return -EINVAL;
 
-	cxlr = __create_region(cxlrd, CXL_DECODER_PMEM, id);
+	cxlr = __create_region(cxlrd, id, CXL_DECODER_PMEM,
+			       CXL_DECODER_HOSTMEM);
 	if (IS_ERR(cxlr))
 		return PTR_ERR(cxlr);
 
@@ -2169,7 +2171,7 @@ static ssize_t create_ram_region_store(struct device *dev,
 	if (rc != 1)
 		return -EINVAL;
 
-	cxlr = __create_region(cxlrd, CXL_DECODER_RAM, id);
+	cxlr = __create_region(cxlrd, id, CXL_DECODER_RAM, CXL_DECODER_HOSTMEM);
 	if (IS_ERR(cxlr))
 		return PTR_ERR(cxlr);
 
@@ -2580,8 +2582,8 @@ construct_region_begin(struct cxl_root_decoder *cxlrd,
 	int err = 0;
 
 	do {
-		cxlr = __create_region(cxlrd, cxled->mode,
-				       atomic_read(&cxlrd->region_id));
+		cxlr = __create_region(cxlrd, atomic_read(&cxlrd->region_id),
+				       cxled->mode, cxled->cxld.target_type);
 	} while (IS_ERR(cxlr) && PTR_ERR(cxlr) == -EBUSY);
 
 	if (IS_ERR(cxlr)) {
