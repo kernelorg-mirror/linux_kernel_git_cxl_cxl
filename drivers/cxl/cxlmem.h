61f6d520258ef0a4e558e7d00442ae9297c6c107
Dan Williams <dan.j.williams@intel.com>
2023-03-22 19:01:17 -0700

    cxl/memdev: Indicate probe deferral
    The first stop for a CXL accelerator driver that wants to establish new
    CXL.mem regions is to register a 'struct cxl_memdev'. That kicks off
    cxl_mem_probe() to enumerate all 'struct cxl_port' instances in the
    topology up to the root.
    
    If the root driver has not attached yet the expectation is that the
    driver waits until that link is established. The common cxl_pci driver
    has reason to keep the 'struct cxl_memdev' device attached to the bus
    until the root driver attaches. An accelerator may want to instead defer
    probing until CXL resources can be acquired.
    
    Use the @endpoint attribute of a 'struct cxl_memdev' to convey when
    accelerator driver probing should be deferred vs failed. Provide that
    indication via a new cxl_acquire_endpoint() API that can retrieve the
    probe status of the memdev.
    
    The first consumer of this API is a test driver that exercises the CXL
    Type-2 flow.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/cxl/cxlmem.h
+++ drivers/cxl/cxlmem.h
@@ -83,6 +83,8 @@ static inline bool is_cxl_endpoint(struct cxl_port *port)
 	return is_cxl_memdev(port->uport);
 }
 
+struct cxl_port *cxl_acquire_endpoint(struct cxl_memdev *cxlmd);
+void cxl_release_endpoint(struct cxl_memdev *cxlmd, struct cxl_port *endpoint);
 struct cxl_memdev *devm_cxl_add_memdev(struct cxl_dev_state *cxlds);
 int devm_cxl_dpa_reserve(struct cxl_endpoint_decoder *cxled,
 			 resource_size_t base, resource_size_t len,
