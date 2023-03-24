554d7c2660aae1199d03174f9437f220ddc5880d
Dan Williams <dan.j.williams@intel.com>
2023-03-24 12:49:06 -0700

    cxl/hdm: Define a driver interface for DPA allocation
    Region creation involves finding available DPA (device-physical-address)
    capacity to map into HPA (host-physical-address) space. Given the HPA
    capacity constraint, define an API, cxl_request_dpa(), that has the
    flexibility to map the minimum amount of memory the driver needs to
    operate vs the total possible that can be mapped given HPA availability.
    
    Factor out the core of cxl_dpa_alloc(), that does free space scanning,
    into a cxl_dpa_freespace() helper, and use that to balance the capacity
    vailable to map vs the @min and @max arguements to cxl_request_dpa().
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/cxl/cxlmem.h
+++ drivers/cxl/cxlmem.h
@@ -89,6 +89,10 @@ struct cxl_memdev *devm_cxl_add_memdev(struct cxl_dev_state *cxlds);
 int devm_cxl_dpa_reserve(struct cxl_endpoint_decoder *cxled,
 			 resource_size_t base, resource_size_t len,
 			 resource_size_t skipped);
+struct cxl_endpoint_decoder *cxl_request_dpa(struct cxl_port *endpoint,
+					     enum cxl_decoder_mode mode,
+					     resource_size_t min,
+					     resource_size_t max);
 
 static inline struct cxl_ep *cxl_ep_load(struct cxl_port *port,
 					 struct cxl_memdev *cxlmd)
